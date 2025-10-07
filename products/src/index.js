d99478465c26b307928544c13d831f84fd158466
Ivan Derlich <a@ivanderlich.com>
2025-10-07 18:17:43 -0300

    Add webpack

---
+++ products/src/index.js
@@ -0,0 +1,11 @@
+import faker from "faker";
+
+let products = "";
+
+for (let i = 0; i < 10; i++) {
+  products += `<div>${faker.commerce.productName()}</div>`;
+}
+
+console.log(products);
+
+// document.querySelector('#dev-products').innerHTML = products;
